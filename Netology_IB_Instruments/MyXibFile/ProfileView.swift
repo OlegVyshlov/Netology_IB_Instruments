9d7d730d692aa568adbdef55a15a91cee3b85ce3
Oleg Vyshlov <vyshlov@gmail.com>
2022-09-29 21:58:10 +0300

    First commit DZ

---
+++ Netology_IB_Instruments/MyXibFile/ProfileView.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by OV on 24.09.2022.
+//  Copyright © 2022 OV. All rights reserved.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    
+    @IBOutlet weak var Name: UILabel!
+    @IBOutlet weak var Birthday: UILabel!
+    @IBOutlet weak var City: UILabel!
+    @IBOutlet weak var Avatar: UIImageView!
+    
+    
+}
---
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by OV on 29.09.2022.
+//  Copyright © 2022 OV. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        
+    }
+    
+}
